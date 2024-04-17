92ce63d5c0cdca63c090a81ee67febd2206d4185
Be-Souza <167259096+Be-Souza@users.noreply.github.com>
2024-04-17 14:41:45 -0300

    Update criaUsuario.py

--- criaUsuario.py
+++ criaUsuario.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # Conectar ao banco de dados SQLite
-conn = sqlite3.connect('databaseTeste.db')
+conn = sqlite3.connect('userSenhaProd.db')
 
 # Criar um cursor para executar comandos SQL
 cursor = conn.cursor()
@@ -10,22 +10,22 @@ cursor = conn.cursor()
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS usuarios (
     id INTEGER PRIMARY KEY,
-    nome TEXT NOT NULL,
+    user TEXT NOT NULL,
     senha TEXT NOT NULL
 )
 ''')
 
 # Pergunta ao usuário, que usuário ele quer criar
 
-userCria = input("Insira o nome de usuário à ser criado:")
-senhaCria = input("Insira a senha numérica do usuário:")
+user = input("Insira o nome de usuário à ser criado:")
+senha = input("Insira a senha do usuário (Texto):")
 
 # Declaração de um metodo
 
-guardarInfo = 'INSERT INTO usuarios (nome, senha) VALUES (?, ?)'
+guardarInfo = 'INSERT INTO usuarios (user, senha) VALUES (?, ?)'
 
 # Inserir dados na tabela
-cursor.execute(guardarInfo,(userCria, senhaCria))
+cursor.execute(guardarInfo,(user, senha))
 
 
 # Salvar as mudanças
@@ -33,7 +33,7 @@ conn.commit()
 
 # Consultar dados
 cursor.execute('SELECT * FROM usuarios')
-print(cursor.fetchall())
+print("Usuário e senha cirados com sucesso!", cursor.fetchall())
 
 # Fechar a conexão
 conn.close()
